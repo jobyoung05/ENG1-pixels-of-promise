227f295abfa53d8c1f6d5e78927d2a8735ec3316
movitzaaron <noraaztivom@gmail.com>
2024-10-21 11:19:51 +0100

    Added files from test project. It draws a random tile map using a .png, can move using mouse.

---
+++ core/src/main/java/com/pixelsOfPromise/Main.java
@@ -0,0 +1,110 @@
+package com.pixelsOfPromise;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main implements ApplicationListener {
+    private TiledMap map;
+    private TiledMapRenderer renderer;
+    private OrthographicCamera camera;
+    private OrthoCamController cameraController;
+    private AssetManager assetManager;
+    private Texture tiles;
+    private Texture texture;
+    private BitmapFont font;
+    private SpriteBatch batch;
+    private final int screenWidth = 300;
+    private final int tileSize = 16;
+
+    @Override
+    public void create() {
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, (width / height) * screenWidth, 300);
+        camera.update();
+
+        cameraController = new OrthoCamController(camera);
+        Gdx.input.setInputProcessor(cameraController);
+
+        font = new BitmapFont();
+        batch = new SpriteBatch();
+
+        {
+            tiles = new Texture(Gdx.files.internal("galletcity.png"));
+            TextureRegion[][] splitTiles = TextureRegion.split(tiles, tileSize, tileSize);
+            map = new TiledMap();
+            MapLayers layers = map.getLayers();
+            for (int l = 0; l < 20; l++) {
+                TiledMapTileLayer layer = new TiledMapTileLayer(150, 150, tileSize, tileSize);
+                for (int x = 0; x < 150; x++) {
+                    for (int y = 0; y < 100; y++) {
+                        int ty = (int)(Math.random() * splitTiles.length);
+                        int tx = (int)(Math.random() * splitTiles[ty].length);
+                        TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+                        cell.setTile(new StaticTiledMapTile(splitTiles[ty][tx]));
+                        layer.setCell(x, y, cell);
+                    }
+                }
+                layers.add(layer);
+            }
+
+            // Initialize the renderer with the map
+            renderer = new OrthogonalTiledMapRenderer(map);
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your application here. The parameters represent the new window size.
+    }
+
+    @Override
+    public void render () {
+        ScreenUtils.clear(100f / 255f, 100f / 255f, 250f / 255f, 1f);
+        camera.update();
+
+        // Render the map
+        renderer.setView(camera);
+        renderer.render();
+
+        // Render the FPS
+        batch.begin();
+        font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20);
+        batch.end();
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy application's resources here.
+        batch.dispose();
+        font.dispose();
+        tiles.dispose();
+        map.dispose();
+    }
+}
