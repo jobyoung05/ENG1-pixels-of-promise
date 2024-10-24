136a8e879599bf58ae4ffd98afd8783da6e121da
jyoung <jobshamandjam@gmail.com>
2024-10-24 14:29:22 +0100

    implement time class

--- core/src/main/java/com/pixelsOfPromise/uniSim/Timer.java
+++ core/src/main/java/com/pixelsOfPromise/uniSim/Timer.java
@@ -3,4 +3,16 @@ package com.pixelsOfPromise.uniSim;
 public class Timer {
     float time;
 
+    public Timer() {
+        this.time = 0;
+    }
+
+    public void add(float deltaTime) {
+        time += deltaTime;
+    }
+
+    public float getTime(){
+        return time;
+    }
+
 }
