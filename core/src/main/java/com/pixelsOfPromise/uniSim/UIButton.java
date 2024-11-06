4a2e13cae5765ffb6594c34b1b72eaa4b7187be2
jobyoung05 <jobyoung05@gmail.com>
2024-11-06 14:29:32 +0000

    create UIButton class

--- core/src/main/java/com/pixelsOfPromise/uniSim/UIButton.java
+++ core/src/main/java/com/pixelsOfPromise/uniSim/UIButton.java
@@ -1,17 +1,33 @@
 package com.pixelsOfPromise.uniSim;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
-import java.awt.*;
 
 public class UIButton {
     private TextButton button;
-    private Skin skin = new Skin();
-    private BitmapFont font = new BitmapFont();
 
-    public UIButton(String text) {
+    public UIButton(String text, Stage stage) {
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style.font = new BitmapFont();
 
+        //Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+        /*
+        BitmapFont font = new BitmapFont();
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
+        skin.addRegions(atlas);
+        style.font = font;
+        style.up = skin.getDrawable("up-button");
+        style.down = skin.getDrawable("down-button");
+        style.checked = skin.getDrawable("checked-button");
+        */
+        button = new TextButton(text, style);
+        button.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        stage.addActor(button);
     }
 }
