c5263cacb21e65e7b6214ab63ac3a3df7b61f346
jobyoung05 <jobyoung05@gmail.com>
2024-11-07 14:05:44 +0000

    Create EndScreen class

---
+++ core/src/main/java/com/pixelsOfPromise/uniSim/EndScreen.java
@@ -0,0 +1,71 @@
+package com.pixelsOfPromise.uniSim;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class EndScreen implements Screen {
+    final UniSim game;
+    OrthographicCamera camera;
+
+    private final int viewportWidth = 600;
+    private final int viewportHeight = 400;
+
+    public EndScreen(final UniSim game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, viewportWidth, viewportHeight);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+        ScreenUtils.clear(0,0,0.2f, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+
+        game.font.draw(game.batch, "Welcome to UniSim!!!", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin.", 100, 100);
+
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
